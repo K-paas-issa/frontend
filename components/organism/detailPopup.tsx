4f347199bc94f7251b4c9aba433162db455eed4c
sik9252 <sik9252@naver.com>
2024-09-02 22:29:40 +0900

    feat: Pin 클릭시 하단 노출 정보 팝업창 추가

--- components/organism/detailPopup.tsx
+++ components/organism/detailPopup.tsx
@@ -1,5 +1,53 @@
 import React from "react";
+import { AreaInfo } from "./map";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "../atom";
 
-export const DetailPopup = () => {
-  return <div>DetailPopup</div>;
+export const DetailPopup = ({ info }: { info: AreaInfo | null }) => {
+  return (
+    info && (
+      <div
+        className={cn(
+          `flex items-center justify-between w-11/12 z-10 absolute bottom-[24px] left-[18px] border border-[2px] bg-white rounded-[8px] px-[20px] py-[16px] cursor-pointer`,
+          info.risk >= 1 && info.risk < 2
+            ? "border-risk_01"
+            : info.risk >= 2 && info.risk < 3
+            ? "border-risk_02"
+            : info.risk >= 3 && info.risk < 4
+            ? "border-risk_03"
+            : info.risk >= 4
+            ? "border-risk_04"
+            : ""
+        )}
+      >
+        <div>
+          <div className="flex items-center">
+            <div
+              className={cn(
+                "font-bold pr-[8px]",
+                info.risk >= 1 && info.risk < 2
+                  ? "text-risk_01"
+                  : info.risk >= 2 && info.risk < 3
+                  ? "text-risk_02"
+                  : info.risk >= 3 && info.risk < 4
+                  ? "text-risk_03"
+                  : info.risk >= 4
+                  ? "text-risk_04"
+                  : ""
+              )}
+            >
+              {info.areaName}
+            </div>
+            <div>위험도 {info.risk}</div>
+          </div>
+          <div className="text-gray_04">
+            신고수: {info.reportCount} / 처리여부: {info.status}
+          </div>
+        </div>
+        <div>
+          <ArrowRight />
+        </div>
+      </div>
+    )
+  );
 };
