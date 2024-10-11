d2fcb1571eae270e6ff8a8f1f1a441464219857b
sik9252 <sik9252@naver.com>
2024-10-11 21:23:06 +0900

    fix: 위험도 색상 직관적으로 수정

--- components/organism/detailPopup.tsx
+++ components/organism/detailPopup.tsx
@@ -1,47 +1,51 @@
 import React from "react";
 import { AreaInfo } from "./kakaoMap";
 import { cn } from "@/lib/utils";
-import { ArrowRight } from "../atom";
+import { ArrowRight, Report } from "../atom";
 
-export const DetailPopup = ({ info }: { info: AreaInfo | null }) => {
+export const DetailPopup = ({ info, onClick }: { info: AreaInfo | null; onClick: () => void }) => {
   return (
     info && (
       <div
         className={cn(
           `flex items-center justify-between w-11/12 z-10 absolute bottom-[24px] left-[18px] border border-[2px] bg-white rounded-[8px] px-[20px] py-[16px] cursor-pointer`,
-          info.risk >= 1 && info.risk < 2
+          info.risk >= 1 && info.risk < 25
             ? "border-risk_01"
-            : info.risk >= 2 && info.risk < 3
+            : info.risk >= 25 && info.risk < 50
             ? "border-risk_02"
-            : info.risk >= 3 && info.risk < 4
+            : info.risk >= 50 && info.risk < 75
             ? "border-risk_03"
-            : info.risk >= 4
+            : info.risk >= 75
             ? "border-risk_04"
             : ""
         )}
+        onClick={onClick}
       >
         <div>
-          <div className="flex items-center">
+          <div className="flex items-center justify-between">
             <div
               className={cn(
                 "font-bold pr-[8px]",
-                info.risk >= 1 && info.risk < 2
+                info.risk >= 1 && info.risk < 25
                   ? "text-risk_01"
-                  : info.risk >= 2 && info.risk < 3
+                  : info.risk >= 25 && info.risk < 50
                   ? "text-risk_02"
-                  : info.risk >= 3 && info.risk < 4
+                  : info.risk >= 50 && info.risk < 75
                   ? "text-risk_03"
-                  : info.risk >= 4
+                  : info.risk >= 75
                   ? "text-risk_04"
                   : ""
               )}
             >
-              {info.areaName}
+              {info.administrative_district}
             </div>
-            <div>위험도 {info.risk}</div>
           </div>
-          <div className="text-gray_04">
-            신고수: {info.reportCount} / 처리여부: {info.status}
+          <div className="flex items-end">
+            <div className="flex items-center gap-[4px] mr-[8px]">
+              <Report color="red" />
+              <div className="pt-[8px]">{info.reportCount}</div>
+            </div>
+            <div> / 위험도 {info.risk}%</div>
           </div>
         </div>
         <div>
