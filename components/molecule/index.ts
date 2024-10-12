227a1f2ff4fcb0e647095b7917edf68885620fe0
sik9252 <sik9252@naver.com>
2024-10-12 17:47:45 +0900

    fix: toast 컴포넌트 제거

--- components/molecule/index.ts
+++ components/molecule/index.ts
@@ -1,7 +1,5 @@
 export { ImageBox } from "./imageBox";
 export { Tooltip } from "./tooltip";
-export { Toaster } from "./toast/toaster";
-export { useToast } from "./toast/use-toast";
 export {
   Dialog,
   DialogPortal,
