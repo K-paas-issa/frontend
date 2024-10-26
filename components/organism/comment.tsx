e18fd1074d7f647e110bec8e2ae4fe68121011fe
sik9252 <sik9252@naver.com>
2024-10-26 16:46:09 +0900

    fix: 댓글 작성 시간 포맷팅

--- components/organism/comment.tsx
+++ components/organism/comment.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import React from "react";
 
 export const Comment = ({ id, content, createdAt }: { id: number; content: string; createdAt: string }) => {
@@ -5,7 +6,7 @@ export const Comment = ({ id, content, createdAt }: { id: number; content: strin
     <div className="flex justify-between items-center border_gray_02 border-b py-[16px]">
       <div>
         <div className="body1 mb-[4px] w-full">{content}</div>
-        <div className="body2 text-gray_04">{createdAt}</div>
+        <div className="body2 text-gray_04">{dayjs(createdAt).format("YYYY.MM.DD HH:mm")}</div>
       </div>
     </div>
   );
